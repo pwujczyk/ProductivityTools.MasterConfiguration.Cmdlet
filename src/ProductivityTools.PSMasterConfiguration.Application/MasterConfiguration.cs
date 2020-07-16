a1f9cf25e8770e506b4dd6589cca9c9fc8c27377
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-07-16 14:32:01 +0200

    assembly resolver projecT

--- src/ProductivityTools.PSMasterConfiguration.Application/MasterConfiguration.cs
+++ src/ProductivityTools.PSMasterConfiguration.Application/MasterConfiguration.cs
@@ -10,18 +10,8 @@ namespace ProductivityTools.PSMasterConfiguration.Application
     {
         public static void GetValue(string value)
         {
-            Console.WriteLine("Hello World!");
-            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-            if (String.IsNullOrWhiteSpace(environment))
-                throw new ArgumentNullException("Environment not found in ASPNETCORE_ENVIRONMENT");
-
-            Console.WriteLine("Environment: {0}", environment);
-
             var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", true, true)
-                .AddJsonFile($"appsettings.{environment}.json", true, true)
-                .AddMasterConfiguration(true)
+                .AddMasterConfiguration("ProductivityTools.PSMasterConfiguration.json",true)
                 .Build();
             var settings = configuration["Login"];
           
