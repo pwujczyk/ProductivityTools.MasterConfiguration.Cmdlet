759dab12865ac35d62854448467e5d2c4a8a2903
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-07-17 20:26:58 +0200

    remove assembly project

--- src/ProductivityTools.PSMasterConfiguration.Cmdlet/AssResolver.cs
+++ src/ProductivityTools.PSMasterConfiguration.Cmdlet/AssResolver.cs
@@ -8,54 +8,54 @@ using System.Text;
 
 namespace ProductivityTools.PSMasterConfiguration.Cmdlet
 {
-    public class MyModuleInitializer : IModuleAssemblyInitializer
-    {
-        public void OnImport()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += DependencyResolution.ResolveNewtonsoftJson;
-        }
-    }
-
-    // Clean up the event handler when the the module is removed
-    // to prevent memory leaks.
-    //
-    // Like IModuleAssemblyInitializer, IModuleAssemblyCleanup allows
-    // you to register code to run when a module is removed (with Remove-Module).
-    // Make sure it is also public with a public parameterless contructor
-    // and implements IModuleAssemblyCleanup.
-    //public class MyModuleCleanup : IModuleAssemblyCleanup
+    //public class MyModuleInitializer : IModuleAssemblyInitializer
     //{
-    //    //public void OnRemove(PSModuleInfo psModuleInfo)
-    //    //{
-    //    //    AppDomain.CurrentDomain.AssemblyResolve -= DependencyResolution.ResolveNewtonsoftJson;
-    //    //}
+    //    public void OnImport()
+    //    {
+    //        AppDomain.CurrentDomain.AssemblyResolve += DependencyResolution.ResolveNewtonsoftJson;
+    //    }
     //}
 
-    internal static class DependencyResolution
-    {
-        private static readonly string s_modulePath = Path.GetDirectoryName(
-            Assembly.GetExecutingAssembly().Location);
+    //// Clean up the event handler when the the module is removed
+    //// to prevent memory leaks.
+    ////
+    //// Like IModuleAssemblyInitializer, IModuleAssemblyCleanup allows
+    //// you to register code to run when a module is removed (with Remove-Module).
+    //// Make sure it is also public with a public parameterless contructor
+    //// and implements IModuleAssemblyCleanup.
+    ////public class MyModuleCleanup : IModuleAssemblyCleanup
+    ////{
+    ////    //public void OnRemove(PSModuleInfo psModuleInfo)
+    ////    //{
+    ////    //    AppDomain.CurrentDomain.AssemblyResolve -= DependencyResolution.ResolveNewtonsoftJson;
+    ////    //}
+    ////}
 
-        public static Assembly ResolveNewtonsoftJson(object sender, ResolveEventArgs args)
-        {
+    //internal static class DependencyResolution
+    //{
+    //    private static readonly string s_modulePath = Path.GetDirectoryName(
+    //        Assembly.GetExecutingAssembly().Location);
 
-            System.Diagnostics.Debugger.Break();
+    //    public static Assembly ResolveNewtonsoftJson(object sender, ResolveEventArgs args)
+    //    {
+
+    //        System.Diagnostics.Debugger.Break();
             
-            // Parse the assembly name
-            var assemblyName = new AssemblyName(args.Name);
+    //        // Parse the assembly name
+    //        var assemblyName = new AssemblyName(args.Name);
 
-            // We only want to handle the dependency we care about.
-            // In this example it's Newtonsoft.Json.
-            if (!assemblyName.Name.Equals("System.Text.Json"))
-            {
-                return null;
-            }
+    //        // We only want to handle the dependency we care about.
+    //        // In this example it's Newtonsoft.Json.
+    //        if (!assemblyName.Name.Equals("System.Text.Json"))
+    //        {
+    //            return null;
+    //        }
 
-            // Generally the version of the dependency you want to load is the higher one,
-            // since it's the most likely to be compatible with all dependent assemblies.
-            // The logic here assumes our module always has the version we want to load.
-            // Also note the use of Assembly.LoadFrom() here rather than Assembly.LoadFile().
-            return Assembly.LoadFrom(Path.Combine(s_modulePath, "System.Text.Json.dll"));
-        }
-    }
+    //        // Generally the version of the dependency you want to load is the higher one,
+    //        // since it's the most likely to be compatible with all dependent assemblies.
+    //        // The logic here assumes our module always has the version we want to load.
+    //        // Also note the use of Assembly.LoadFrom() here rather than Assembly.LoadFile().
+    //        return Assembly.LoadFrom(Path.Combine(s_modulePath, "System.Text.Json.dll"));
+    //    }
+    //}
 }
