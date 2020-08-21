a964ff03168dedf83a807ea98ab8fed8e2b09200
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-08-21 20:17:17 +0200

    clearing the projecT

---
+++ ProductivityTools.PSMasterConfiguration.Cmdlet/GetMasterConfiguration/Commands/GetConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ProductivityTools.PSMasterConfiguration.Cmdlet.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.PSMasterConfiguration.Cmldet.Commands
+{
+    public class GetConfiguration : PSCmdlet.PSCommandPT<GetMasterConfiguration>
+    {
+        protected override bool Condition => true;
+
+        public GetConfiguration(GetMasterConfiguration getMasterConfiguration) : base(getMasterConfiguration) { }
+
+        protected override void Invoke()
+        {
+            var config=MasterConfiguration.GetValue(this.Cmdlet.Key);
+            this.Cmdlet.WriteOutput(config);
+            
+        }
+    }
+}
+
+
